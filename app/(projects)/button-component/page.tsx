39ead79b40dc4ea2aff9348575b62881644b65ab
Chance Hernandez <chance.hernandez24@gmail.com>
2024-12-12 12:24:22 -0800

    button component complete

--- app/(projects)/button-component/page.tsx
+++ app/(projects)/button-component/page.tsx
@@ -1,45 +1,287 @@
-'use client';
+"use client";
 
 import Button from "@/components/common/Button";
 
 export default function Page() {
-  return (
-    <div className="py-[112px] bg-white">
-      <div className="flex flex-col gap-12 items-center">
-        <div className="flex items-center gap-5">
-          <Button
-            text="Button CTA"
-            onClick={() => {}}
-            size={"M"}
-            type={"primary"}
-          />
-          <Button
-            text="Button CTA"
-            onClick={() => {}}
-            size={"L"}
-            type={"primary"}
-          />
-          <Button
-            text="Button CTA"
-            onClick={() => {}}
-            size={"XL"}
-            type={"primary"}
-          />
-          <Button
-            text="Button CTA"
-            onClick={() => {}}
-            size={"2XL"}
-            type={"primary"}
-          />
-          <Button
-            text="Button CTA"
-            onClick={() => {}}
-            size={"2XL"}
-            type={"primary"}
-            iconOnlySrc="img/star-line.svg"
-          />
+    return (
+        <div className="px-6 md:px-8 lg:px-0 flex justify-center items-start py-[112px]">
+            <div className="flex flex-col gap-12">
+                <div className="flex items-center gap-5">
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"M"}
+                        style={"primary"}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"L"}
+                        style={"primary"}
+                        leftSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"XL"}
+                        style={"primary"}
+                        rightSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"primary"}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"primary"}
+                        svgOnly={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                </div>
+                <div className="flex items-center gap-5">
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"M"}
+                        style={"secondary"}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"L"}
+                        style={"secondary"}
+                        leftSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"XL"}
+                        style={"secondary"}
+                        rightSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"secondary"}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"secondary"}
+                        svgOnly={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                </div>
+                <div className="flex items-center gap-5">
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"M"}
+                        style={"tertiary"}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"L"}
+                        style={"tertiary"}
+                        leftSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"XL"}
+                        style={"tertiary"}
+                        rightSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"tertiary"}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"tertiary"}
+                        svgOnly={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                </div>
+                <div className="flex items-center gap-5">
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"M"}
+                        style={"link"}
+                        href="/"
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"L"}
+                        style={"link"}
+                        href="/"
+                        leftSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"XL"}
+                        style={"link"}
+                        href="/"
+                        rightSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"link"}
+                        href="/"
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"link"}
+                        href="/"
+                        svgOnly={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                </div>
+                <div className="flex items-center gap-5">
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"M"}
+                        style={"link-gray"}
+                        href="/"
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"L"}
+                        style={"link-gray"}
+                        href="/"
+                        leftSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"XL"}
+                        style={"link-gray"}
+                        href="/"
+                        rightSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"link-gray"}
+                        href="/"
+                        disabled={true}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"link-gray"}
+                        href="/"
+                        svgOnly={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                </div>
+                <div className="flex items-center gap-5">
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"M"}
+                        style={"destructive"}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"L"}
+                        style={"destructive"}
+                        leftSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"XL"}
+                        style={"destructive"}
+                        rightSVG={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"destructive"}
+                    />
+                    <Button
+                        text="Button CTA"
+                        onClick={() => {}}
+                        size={"2XL"}
+                        style={"destructive"}
+                        svgOnly={{
+                            fill: "currentColor",
+                            path: "M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z",
+                        }}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 interface ButtonProps {
     text: string;
     onClick: () => void;
     size: "M" | "L" | "XL" | "2XL";
-    type: "primary" | "secondary" | "tertiary" | "destructive";
-    leftIconSrc?: string;
-    rightIconSrc?: string;
-    iconOnlySrc?: string;
+    style:
+        | "primary"
+        | "secondary"
+        | "tertiary"
+        | "destructive"
+        | "link"
+        | "link-gray";
+    href?: string;
+    leftSVG?: { fill: string; path: string };
+    rightSVG?: { fill: string; path: string };
+    svgOnly?: { fill: string; path: string };
     disabled?: boolean;
 }
 
@@ -15,10 +23,11 @@ const Button: React.FC<ButtonProps> = ({
     text,
     onClick,
     size,
-    type,
-    leftIconSrc,
-    rightIconSrc,
-    iconOnlySrc,
+    style,
+    href,
+    leftSVG,
+    rightSVG,
+    svgOnly,
     disabled,
 }) => {
     const buttonSizeMap = {
@@ -28,6 +37,13 @@ const Button: React.FC<ButtonProps> = ({
         "2XL": "gap-2.5 px-6 py-4 text-lg",
     };
 
+    const linkSizeMap = {
+        M: "gap-1 text-sm",
+        L: "gap-1.5 text-base",
+        XL: "gap-1.5 text-base",
+        "2XL": "gap-2.5 text-lg",
+    };
+
     const iconOnlySizeMap = {
         M: "px-2.5 py-2.5",
         L: "px-3 py-3",
@@ -35,28 +51,98 @@ const Button: React.FC<ButtonProps> = ({
         "2XL": "px-4 py-4",
     };
 
-    const typeMap = {
+    const styleMap = {
         primary:
-            "bg-indigo-700 text-white hover:bg-indigo-800 focus:bg-indigo-800 focus:ring focus:ring-[#444CE7]/[.12] focus:outline-none",
-        secondary: "",
-        tertiary: "",
-        destructive: "",
+            "bg-indigo-700 text-white hover:bg-indigo-800 focus:bg-indigo-800 focus:ring focus:ring-[#444CE7]/[.12] focus:outline-none disabled:text-neutral-400 disabled:bg-neutral-100",
+        secondary:
+            "bg-white text-neutral-900 border border-neutral-200 hover:bg-neutral-50 focus:bg-neutral-50 focus:ring focus:ring-[#444CE7]/[.12] focus:outline-none disabled:text-neutral-400 disabled:bg-neutral-100",
+        tertiary:
+            "bg-transparent text-indigo-700 drop-shadow-none hover:bg-neutral-50 focus:bg-neutral-50 focus:ring focus:ring-[#444CE7]/[.12] focus:outline-none disabled:text-neutral-400 hover:disabled:bg-transparent",
+        destructive:
+            "bg-red-600 text-white hover:bg-red-700 focus:bg-red-700 focus:ring focus:ring-[#444CE7]/[.12] focus:outline-none disabled:text-neutral-400 disabled:bg-transparent disabled:drop-shadow-none hover:disabled:bg-transparent",
+        link: "text-indigo-700 focus:ring focus:ring-[#444CE7]/[.12] focus:outline-none",
+        "link-gray": "text-neutral-600 focus:ring focus:ring-[#444CE7]/[.12] focus:outline-none",
     };
 
-    if (iconOnlySrc) {
+    if (href) {
+        if (svgOnly) {
+            const { fill, path } = svgOnly;
+    
+            return (
+                <Link
+                    href={href}
+                    className={`flex items-center ${linkSizeMap[size]} ${styleMap[style]} p-0 rounded cursor-pointer aria-disabled:cursor-not-allowed aria-disabled:text-neutral-400`}
+                    aria-disabled={disabled}
+                    tabIndex={disabled ? -1 : undefined}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill={fill}
+                        width={17}
+                        height={17}
+                    >
+                        <path d={path}></path>
+                    </svg>
+                </Link>
+            );
+        }
+
+        return (
+            <Link
+                href={href}
+                className={`flex items-center ${linkSizeMap[size]} ${styleMap[style]} p-0 text-medium whitespace-nowrap rounded cursor-pointer aria-disabled:cursor-not-allowed aria-disabled:text-neutral-400`}
+                aria-disabled={disabled}
+                tabIndex={disabled ? -1 : undefined}
+            >
+                {leftSVG ? (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill={leftSVG.fill}
+                        width={17}
+                        height={17}
+                    >
+                        <path d={leftSVG.path}></path>
+                    </svg>
+                ) : (
+                    ""
+                )}
+                <p className="px-0.5">{text}</p>
+                {rightSVG ? (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill={rightSVG.fill}
+                        width={17}
+                        height={17}
+                    >
+                        <path d={rightSVG.path}></path>
+                    </svg>
+                ) : (
+                    ""
+                )}
+            </Link>
+        );
+    }
+
+    if (svgOnly) {
+        const { fill, path } = svgOnly;
+
         return (
             <button
                 onClick={() => onClick}
-                className={`flex items-center ${iconOnlySizeMap[size]} ${typeMap[type]} rounded drop-shadow-sm cursor-pointer`}
+                className={`flex items-center ${iconOnlySizeMap[size]} ${styleMap[style]} rounded drop-shadow cursor-pointer disabled:cursor-not-allowed`}
+                disabled={disabled}
             >
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
-                    fill="currentColor"
+                    fill={fill}
                     width={17}
                     height={17}
                 >
-                    <path d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z"></path>
+                    <path d={path}></path>
                 </svg>
             </button>
         );
@@ -65,9 +151,36 @@ const Button: React.FC<ButtonProps> = ({
     return (
         <button
             onClick={() => onClick}
-            className={`flex items-center ${buttonSizeMap[size]} ${typeMap[type]} text-medium whitespace-nowrap rounded drop-shadow-sm cursor-pointer`}
+            className={`flex items-center ${buttonSizeMap[size]} ${styleMap[style]} text-medium whitespace-nowrap rounded drop-shadow cursor-pointer disabled:cursor-not-allowed`}
+            disabled={disabled}
         >
-            {text}
+            {leftSVG ? (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill={leftSVG.fill}
+                    width={17}
+                    height={17}
+                >
+                    <path d={leftSVG.path}></path>
+                </svg>
+            ) : (
+                ""
+            )}
+            <p className="px-0.5">{text}</p>
+            {rightSVG ? (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill={rightSVG.fill}
+                    width={17}
+                    height={17}
+                >
+                    <path d={rightSVG.path}></path>
+                </svg>
+            ) : (
+                ""
+            )}
         </button>
     );
 };
